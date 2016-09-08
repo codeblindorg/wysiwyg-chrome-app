2da4189007a577e95e6c52c6380f210639f9abbd
Joseph Parkton <creekhippybear@gmail.com>
2016-09-08 09:07:32 -0700

    Updating builder.html

--- js/builder.js
+++ js/builder.js
@@ -2,3 +2,14 @@ $("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
+
+$(document).ready(function(){
+  
+  $("#file-actions-add-folder").on("click",function(){
+    console.log( "Beep, Boop, making folders :P" )
+  })
+  $("#file-actions-add-file").on("click",function(){
+    console.log( "Beep, Boop, making files :P" )
+  })
+  
+})
